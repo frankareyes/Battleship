289053533eb81d6fae79652615e2dcba8504fa3a
fabreure <fabreure@W7-2016-SEC-87.isimtl.local>
2019-02-23 17:35:15 -0500

    deuxieme

---
+++ project/src/edu/battleship/modele/Grille.java
@@ -0,0 +1,24 @@
+package edu.battleship.modele;
+
+public class Grille {
+	private int[][] pos;
+	
+	public int[][] getPos() {
+		return pos;
+	}
+
+	public void setPos(int[][] pos) {
+		this.pos = pos;
+	}
+
+	public Grille() {
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	public Grille(int[][] pos) {
+		super();
+		this.pos = pos;
+	}
+
+}
---
+++ project/src/edu/battleship/modele/Joueur.java
@@ -0,0 +1,65 @@
+package edu.battleship.modele;
+
+import java.util.List;
+
+public class Joueur {
+	private String nom;
+	private int points;
+	private List<Bateau> bateaux;
+	private Grille grilleNavale;
+	private boolean tour;
+	
+	public Joueur(String nom, int points, List<Bateau> bateaux, Grille grilleNavale, boolean tour) {
+		super();
+		this.nom = nom;
+		this.points = points;
+		this.bateaux = bateaux;
+		this.grilleNavale = grilleNavale;
+		this.tour = tour;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public List<Bateau> getBateaux() {
+		return bateaux;
+	}
+
+	public void setBateaux(List<Bateau> bateaux) {
+		this.bateaux = bateaux;
+	}
+
+	public Grille getGrilleNavale() {
+		return grilleNavale;
+	}
+
+	public void setGrilleNavale(Grille grilleNavale) {
+		this.grilleNavale = grilleNavale;
+	}
+
+	public boolean isTour() {
+		return tour;
+	}
+
+	public void setTour(boolean tour) {
+		this.tour = tour;
+	}
+
+	public Joueur() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
