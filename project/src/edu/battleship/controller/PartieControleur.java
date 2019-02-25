ce000dccc7f002a47db2ea90176baef1bfec5b39
fabreure <fabreure@W7-2016-SEC-87.isimtl.local>
2019-02-24 19:30:57 -0500

    ...

--- project/src/edu/battleship/controller/PartieControleur.java
+++ project/src/edu/battleship/controller/PartieControleur.java
@@ -1,4 +1,4 @@
-package edu.battleship.controller;
+/*package edu.battleship.controller;
 
 import edu.battleship.modele.Host;
 import edu.battleship.modele.Joueur;
@@ -9,6 +9,32 @@ public class PartieControleur {
 
 	Joueur joueur1 = new CreerJoueurControleur().creerJoueur(nomJoueur1, host);
 	Joueur joueur2 = new CreerJoueurControleur().creerJoueur(nomJoueur2, host);
+	}
+
+}*/
+
+/**
+ * Class to 
+ */
+package edu.battleship.controller;
+
+import edu.battleship.modele.Host;
+import edu.battleship.modele.Joueur;
+
+public class PartieControleur {
+
+	private Joueur joueur;
+	private Joueur host;
+	private Joueur remoteJoueur;
+
+	public PartieControleur(String nomJoueur1, String nomJoueur2, Host host, String mode) {
+
+		Joueur joueur1 = new CreerJoueurControleur().creerJoueur(nomJoueur1, host);
+		Joueur joueur2 = new CreerJoueurControleur().creerJoueur(mode.equalsIgnoreCase("machine") ? "machine" : nomJoueur2, host);
+	}
+
+	public Joueur obtenirJoueur(String nomJoueur, String mode, Host host) {
+		return new CreerJoueurControleur().creerJoueur(mode.equalsIgnoreCase("machine") ? "machine" : nomJoueur, host);
 	}
 
 }
